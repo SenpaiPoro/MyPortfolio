cce43de77b04c5e66783c525c0d1613cc4ca140e
Marlon Jr Mullet <marlonmullet10@gmail.com>
2026-03-25 16:25:25 +0800

    used the existing data un the update per input

--- Portfolio_Dashboard/resume.php
+++ Portfolio_Dashboard/resume.php
@@ -23,35 +23,35 @@
 
                     <div class="mb-3">
                         <label> Heading</label>
-                        <input type="text" name="name" require class="form-control">
+                        <input type="text" name="name" require class="form-control"value="<?php echo $row['heading']?>">
                     </div>
                     <div class="mb-3">
                         <label> Support</label>
-                        <input type="text" name="support" require class="form-control">
+                        <input type="text" name="support" require class="form-control" value="<?php echo $row['support']?>">
                     </div>
                     <div class="mb-3">
                         <label> Tagline</label>
-                        <input type="text" name="tagline" require class="form-control">
+                        <input type="text" name="tagline" require class="form-control" value="<?php echo $row['tagline']?>">
                     </div>
                     <div class="mb-3">
                         <label> Bio</label>
-                        <textarea name="bio" require class="form-control" rows="3"></textarea>
+                        <textarea name="bio" require class="form-control" rows="3"></textarea value="<?php echo $row['bio']?>">
                     </div>
                     <div class="mb-3">
                         <label for="formFile" class="form-label">Profile Picture</label>
-                        <input class="form-control" type="file" name="image" id="fileInput" require>
+                        <input class="form-control" type="file" name="image" id="fileInput" require value="<?php echo $row['profile']?>">
                     </div>
                     <div class="mb-3">
                         <label> Instagram</label>
-                        <input type="text" name="instagram" require class="form-control">
+                        <input type="text" name="instagram" require class="form-control" value="<?php echo $row['ig_link']?>">
                     </div>
                     <div class="mb-3">
                         <label> Linkin</label>
-                        <input type="text" name="linkin" require class="form-control">
+                        <input type="text" name="linkin" require class="form-control" value="<?php echo $row['in_link']?>">
                     </div>
                     <div class="mb-3">
                         <label> Github</label>
-                        <input type="text" name="github" require class="form-control">
+                        <input type="text" name="github" require class="form-control" value="<?php echo $row['github']?>">
                     </div>
                     <div class="mb-3 text-end">
                         <button type="submit" name="profile" class="btn btn-primary">Update Profile</button>
