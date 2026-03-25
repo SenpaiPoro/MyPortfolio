c4949c8258392f394fd07121a44481a64644f6ec
Marlon Jr Mullet <marlonmullet10@gmail.com>
2026-03-25 16:04:06 +0800

    bold the name

--- Portfolio_Dashboard/include/topbar.php
+++ Portfolio_Dashboard/include/topbar.php
@@ -175,7 +175,7 @@
                         <li class="nav-item dropdown no-arrow">
                             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
                                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <span class="mr-2 d-none d-lg-inline text-gray-600 small"><?php echo $row['name'];?></span>
+                                <span class="mr-2 d-none d-lg-inline text-gray-600 small"><b><?php echo $row['name'];?></b></span>
                                 <img class="img-profile rounded-circle"
                                     src="<?php 
                                     if($row['profile'] == null){
