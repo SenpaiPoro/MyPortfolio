5e75b5df09dd64e9366f8c866609befbd244311f
Marlon Jr Mullet <marlonmullet10@gmail.com>
2026-03-31 06:35:34 +0800

    modify and fixed the profile dashboard

--- Portfolio_Dashboard/profile.php
+++ Portfolio_Dashboard/profile.php
@@ -21,36 +21,36 @@
                     <input name="username" type="hidden" value="<?php echo $username; ?>">
 
                     <div class="mb-3">
-                        <label> Heading</label>
-                        <input type="text" name="heading" require class="form-control">
+                        <label> <b>Heading</b></label>
+                        <input type="text" name="heading" require class="form-control" value="<?php echo $row['heading'];?>">
                     </div>
                     <div class="mb-3">
-                        <label> Support</label>
-                        <input type="text" name="support" require class="form-control">
+                        <label> <b> Support </b></label>
+                        <input type="text" name="support" require class="form-control" value="<?php echo $row['support'];?>">
                     </div>
                     <div class="mb-3">
-                        <label> Tagline</label>
-                        <input type="text" name="tagline" require class="form-control">
+                        <label> <b>Tagline </b></label>
+                        <input type="text" name="tagline" require class="form-control" value="<?php echo $row['tagline'];?>">
                     </div>
                     <div class="mb-3">
-                        <label> Bio</label>
-                        <textarea name="bio" require class="form-control" rows="3"></textarea>
+                        <label> <b>Bio</b></label>
+                        <textarea name="bio" require class="form-control" rows="3" value="<?php echo $row['bio'];?>"><?php echo $row['bio'];?>"</textarea>
                     </div>
                     <div class="mb-3">
-                        <label for="formFile" class="form-label">Profile Picture</label>
-                        <input class="form-control" type="file" name="image" id="fileInput" require>
+                        <label for="formFile" class="form-label"><b>Profile Picture</b></label>
+                        <input class="form-control" type="file" name="image" id="fileInput" require value="<?php echo $row['profile'];?>">
                     </div>
                     <div class="mb-3">
-                        <label> Instagram</label>
-                        <input type="text" name="instagram" require class="form-control">
+                        <label><b> Instagram</b></label>
+                        <input type="text" name="instagram" require class="form-control"value="<?php echo $row['ig_link'];?>">
                     </div>
                     <div class="mb-3">
-                        <label> Linkin</label>
-                        <input type="text" name="linkin" require class="form-control">
+                        <label><b> Linkin</b></label>
+                        <input type="text" name="linkin" require class="form-control" value="<?php echo $row['in_link'];?>">
                     </div>
                     <div class="mb-3">
-                        <label> Github</label>
-                        <input type="text" name="github" require class="form-control">
+                        <label><b> Github</b></label>
+                        <input type="text" name="github" require class="form-control" value="<?php echo $row['github'];?>">
                     </div>
                     <div class="mb-3 text-end">
                         <button type="submit" name="profile" class="btn btn-primary">Update Profile</button>
