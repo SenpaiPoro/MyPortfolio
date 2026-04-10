16dba15b8dce54c5fd859969e0201ac6842af8f8
Marlon Jr Mullet <marlonmullet10@gmail.com>
2026-04-10 16:13:53 +0800

    create a layout on edit resume, will modify soon

--- Portfolio_Dashboard/resume_edit.php
+++ Portfolio_Dashboard/resume_edit.php
@@ -1,7 +1,62 @@
 <?php include ('include/sidebar.php'); ?>
 <?php include ('include/topbar.php'); ?>
 
+        <div class="row"></div>
+ <div class="col-md-12">
+        <div class="card">
+            <div class="card-header">
+                <h4>
+                    <b>Edit Resume</b>
+                     <a href="resumelist.php" class="btn btn-danger float-end"> Back </a> 
+                </h4>
 
+            </div>
+            <div class="card-body">
+              
+
+                <form action="../config/code.php" method="POST" enctype="multipart/form-data">
+
+                <div class="form-floating">
+                </div>
+
+                 <label>Type:</label>
+                        <select class="form-select form-select-lg mb-3" aria-label="Large select example" name="type">
+                            <option value="Experience">
+                                Experience
+                            </option>
+                            <option value="Education">
+                                Education
+                            </option>
+                        </select>
+                    <div class="mb-3">
+                        <label> Name of Company/School</label>
+                        <input type="text" name="name" require class="form-control">
+                    </div>
+                    <div class="mb-3">
+                        <label> Title: </label>
+                        <input type="text" name="title" require class="form-control" >
+                    </div>
+                    <div class="mb-3">
+                        <label> Address: </label>
+                        <input type="text" name="address" require class="form-control" >
+                    </div>
+                    <div class="mb-3">
+                        <label> Description: </label>
+                        <textarea name="description" require class="form-control" rows="3"></textarea>
+                    </div>
+                    <div class="mb-3">
+                        <label> Year: </label>
+                        <input type="number" name="year" require class="form-control" >
+                    </div>
+                    <div class="mb-3 text-end">
+                        <button type="submit" name="Update" class="btn btn-primary">Update</button>
+                    </div>
+                </form>
+
+            </div>
+        </div>
+    </div>
+                    
 
 
 
