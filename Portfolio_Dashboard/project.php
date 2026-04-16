706dca10ccd8d6286b858965630fff8736d4e5a9
Marlon Jr Mullet <marlonmullet10@gmail.com>
2026-04-16 15:48:45 +0800

    change and set what all needed on the projects

--- Portfolio_Dashboard/project.php
+++ Portfolio_Dashboard/project.php
@@ -7,7 +7,7 @@
         <div class="card">
             <div class="card-header">
                 <h4>
-                    Profile Managerment
+                    Project Managerment
                 </h4>
             </div>
             <div class="card-body">
@@ -21,39 +21,15 @@
                 <input type="hidden" name="colleges" value="<?php echo $college?>">
 
                     <div class="mb-3">
-                        <label> Heading</label>
+                        <label> Name</label>
                         <input type="text" name="name" require class="form-control">
                     </div>
                     <div class="mb-3">
-                        <label> Support</label>
+                        <label> Description</label>
                         <input type="text" name="support" require class="form-control">
                     </div>
-                    <div class="mb-3">
-                        <label> Tagline</label>
-                        <input type="text" name="tagline" require class="form-control">
-                    </div>
-                    <div class="mb-3">
-                        <label> Bio</label>
-                        <textarea name="bio" require class="form-control" rows="3"></textarea>
-                    </div>
-                    <div class="mb-3">
-                        <label for="formFile" class="form-label">Profile Picture</label>
-                        <input class="form-control" type="file" name="image" id="fileInput" require>
-                    </div>
-                    <div class="mb-3">
-                        <label> Instagram</label>
-                        <input type="text" name="instagram" require class="form-control">
-                    </div>
-                    <div class="mb-3">
-                        <label> Linkin</label>
-                        <input type="text" name="linkin" require class="form-control">
-                    </div>
-                    <div class="mb-3">
-                        <label> Github</label>
-                        <input type="text" name="github" require class="form-control">
-                    </div>
                     <div class="mb-3 text-end">
-                        <button type="submit" name="profile" class="btn btn-primary">Update Profile</button>
+                        <button type="submit" name="profile" class="btn btn-primary">Add Project</button>
                     </div>
                 </form>
 
