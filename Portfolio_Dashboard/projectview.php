1787e155c07b26e7004cd5fed7d74cf4219bd72a
Marlon Jr Mullet <marlonmullet10@gmail.com>
2026-04-27 10:42:09 +0800

    add file for project view

---
+++ Portfolio_Dashboard/projectview.php
@@ -0,0 +1,9 @@
+
+<?php include ('include/sidebar.php'); ?>
+<?php include ('include/topbar.php'); ?>
+
+
+
+
+<script src="../admin/assets/js/script.js"></script>
+<?php include ('include/footer.php'); ?>
