be3fa3a51395449a9b1b633d3cab5116ed64d7ad
Marlon Jr Mullet <marlonmullet10@gmail.com>
2026-05-17 11:11:19 +0800

    put the list of messages on the accounts

--- Portfolio_Dashboard/messages.php
+++ Portfolio_Dashboard/messages.php
@@ -1,7 +1,71 @@
       <?php include ('include/sidebar.php'); ?>
       <?php include ('include/topbar.php'); ?>
 
-      
+      <div class="row">
+    <div class="col-md-12">
+        <div class="card">
+            <div class="card-header">
+                <h4><b>Messages</b></h4>
+                  <br>
+                    <a href="project.php" class="btn btn-primary float-end"> Add Project </a>
+                </h4> 
+            </div>
+        </div>
+        <div class="card-body">
+            <form class="d-flex" role="search" method="GET">
+            <div class="input-group input-group-sm mb-3">
+</div>
+</form>
+            <table class="table table-bordered table-striped table-hover">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Phone</th>
+                        <th>Message</th>
+                    </tr>
+                </thead>
+                <tbody class="table-hover">
+                <?php
+
+                        $Data = Getdata("messages", $id );
+                    
+
+                    if (mysqli_num_rows($Data) > 0) {
+                        foreach ($Data as $DataList) {
+                ?>
+                              <tr>
+                                    <td> <?= htmlspecialchars($DataList['name']); ?></td>
+                                    <td> <?= htmlspecialchars($DataList['email']); ?></td>
+                                    <td> <?= htmlspecialchars($DataList['phone']); ?></td>
+                                    
+                                <td> 
+                                <a href="include/deleteproject.php?id=<?= $DataList['id'];?> "class="btn btn-danger btn-sm" onclick="return confirm('Are you Sure that you want to delete this Project? ');">Delete</a>
+                                <a href="projectview.php?id=<?= $DataList['id'];?>   "class="btn btn-info btn-sm">View Messages</a>
+                            </td>
+                    </tr>
+                            <?php
+                        }
+                    }
+                    else
+                    {
+                        ?>
+                            <tr>
+                                <td colspan="4">
+                                    No Record!
+                                </td>
+                            </tr>
+                        <?php
+                    }
+                  ?>
+
+                </tbody>
+            </table>
+
+        </div>
+    </div>
+</div>
+<script src="../admin/assets/js/script.js"></script>
 
 
       
